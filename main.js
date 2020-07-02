8b84f07820403a200aa51d4d3d3e2bfdf09c0cfa
rick427 <rchardnjoku@yahoo.com>
2020-07-02 11:23:49 +0100

    added nav shadow

--- main.js
+++ main.js
@@ -1,9 +1,20 @@
+// initialize animate on scroll library
 AOS.init();
 
+// select DOM elements
 const body = document.querySelector('body');
 const header = document.querySelector('header');
+const headerNav = document.querySelector('nav');
+console.log(headerNav)
 const checkbox = document.querySelector('input[name=theme]');
 
+// Attach event listener to the window object to check if the scroll on 
+// on the Y-direction is greater than 0. i.e the user has scrolled
+window.addEventListener('scroll', () => {
+    headerNav.classList.toggle('sticky', window.scrollY > 0);
+});
+
+// check if something exists already in LS
 if(localStorage.getItem('theme') === 'dark-mode'){
     body.classList.toggle(localStorage.getItem('theme'));
     checkbox.checked = true;
@@ -11,6 +22,7 @@ if(localStorage.getItem('theme') === 'dark-mode'){
     header.style.backgroundSize = 'cover';
 }
 
+// add event listener to enable and disable themes
 checkbox.addEventListener('change', event => {
     if(event.target.checked){
         body.classList.add('dark-mode');
@@ -25,3 +37,4 @@ checkbox.addEventListener('change', event => {
         localStorage.removeItem('theme');
     }
 });
+
